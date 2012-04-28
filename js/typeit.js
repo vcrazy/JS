2cd60c4f7f384fd72c101c1da2ed70dec67c86e4
V <vcrazy@abv.bg>
2012-04-28 14:37:01 +0300

    vtype plugin

---
+++ js/typeit.js
@@ -0,0 +1,23 @@
+$.vtype = function(word, callback, args) {
+	var curr = '';
+	word = word.toLowerCase();
+
+	$('body').keydown(function(e) {
+	if(!args) args=[]; // IE barks when args is null
+		var code = (e.keyCode ? e.keyCode : e.which);
+		var character = String.fromCharCode(code).toLowerCase();
+
+		if(character == word.charAt(curr.length)){
+			curr += character;
+		}else{
+			curr = '';
+		}
+
+		if(curr.length == word.length){
+			curr = '';
+			callback();
+		}
+
+		return true;
+	});
+};
